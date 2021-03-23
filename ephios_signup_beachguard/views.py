897988fdb0947c7b6abb7302aabf67500268f3ba
Julian B <jeriox@users.noreply.github.com>
2021-03-23 01:09:58 +0100

    export complete table

--- ephios_signup_beachguard/views.py
+++ ephios_signup_beachguard/views.py
@@ -1,21 +1,23 @@
 import io
 
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.forms import HiddenInput, Field
 from django.http import FileResponse
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.utils.formats import date_format
 from django.utils.translation import gettext as _
-from django.views import View
 from django.views.generic import FormView
 from dynamic_preferences.registries import global_preferences_registry
 from ephios.core.models import Shift, AbstractParticipation
 from ephios.core.views.settings import SettingsViewMixin
 from ephios.plugins.basesignup.signup.section_based import SectionBasedConfigurationForm
+from reportlab.lib import colors
 from reportlab.lib.pagesizes import A4, landscape
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.units import cm
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle
 
 from ephios_signup_beachguard.signup import BeachguardSignupMethod
 
@@ -37,6 +39,7 @@ class BeachguardSectionSettingsView(SettingsViewMixin, FormView):
         return redirect(reverse("signup_beachguard:sections"))
 
 
+@login_required
 def pdf_export(request, *args, **kwargs):
     buffer = io.BytesIO()
     p = SimpleDocTemplate(
@@ -49,21 +52,53 @@ def pdf_export(request, *args, **kwargs):
         bottomMargin=1 * cm,
     )
     style = getSampleStyleSheet()
-    story = [
-        Paragraph("Events", style["Heading1"]),
-        Spacer(height=0.5 * cm, width=19 * cm),
-    ]
-    shifts = Shift.objects.filter(signup_method_slug=BeachguardSignupMethod.slug)
-    sections = global_preferences_registry.manager()["beachguard__sections"]
+    story = []
+    shifts = Shift.objects.filter(signup_method_slug=BeachguardSignupMethod.slug).order_by("start_time")
 
-    for shift in shifts:
-        story.append(Paragraph(shift.get_start_end_time_display(), style["Heading2"]))
-        for section in sections:
-            section_participants = [f"{participation.participant.first_name} {participation.participant.last_name}" for participation in AbstractParticipation.objects.filter(
+    # create a matrix of sections and shifts which contain the corresponding participations.
+    # this is a separate step to calculate the number of rows we need for each section.
+    participations = {}
+    for section in global_preferences_registry.manager()["beachguard__sections"]:
+        participations[section["uuid"]] = {}
+        for shift in shifts:
+            participations[section["uuid"]][shift.pk] = AbstractParticipation.objects.filter(
                 shift=shift, data__dispatched_section_uuid=section["uuid"]
-            )]
-            story.append(Paragraph(f"{section['title']}: {', '.join(section_participants)}"))
+            )
+        participations[section["uuid"]]["row_count"] = max(len(max(participations[section["uuid"]].values(), key=lambda key: len(key))), section["min_count"])
+        participations[section["uuid"]]["title"] = section["title"]
 
+    # sort the sections ascending by row_count to create a nice look in the export
+    participations = {k: v for k, v in sorted(participations.items(), key=lambda item: item[1]["row_count"])}
+
+    # build the table header with the section names
+    columns = []
+    header = [""]
+    for section_uuid, section_participations in participations.items():
+        header.extend([Paragraph(section_participations["title"])] * section_participations["row_count"])
+    columns.append(header)
+
+    # finally build the table contents
+    for shift in shifts:
+        column = [Paragraph(date_format(shift.start_time, "SHORT_DATE_FORMAT"))]
+        for section_uuid, section_participations in participations.items():
+            participant_rows = []
+            for participation in section_participations[shift.pk]:
+                participant_rows.append(Paragraph(f"{participation.participant.first_name} {participation.participant.last_name}"))
+            if len(participant_rows) < section_participations["row_count"]:
+                participant_rows.extend([Paragraph("")] * (section_participations["row_count"] - len(participant_rows)))
+            column.extend(participant_rows)
+        columns.append(column)
+
+    table = Table(columns)
+    table.setStyle(
+        TableStyle(
+            [
+                ("INNERGRID", (0, 0), (-1, -1), 0.25, colors.black),
+                ("BOX", (0, 0), (-1, -1), 0.25, colors.black),
+            ]
+        )
+    )
+    story.append(table)
     p.build(story)
     buffer.seek(0)
     return FileResponse(buffer, as_attachment=False, filename="beachguard.pdf")
