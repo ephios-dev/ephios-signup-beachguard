f0cb084d1572d3eb5516eda8e6981e995cdc20af
Felix Rindt <felix@rindt.me>
2023-07-13 17:56:25 +0200

    translate plugin name

--- ephios_signup_beachguard/apps.py
+++ ephios_signup_beachguard/apps.py
@@ -1,13 +1,13 @@
 from ephios.core.plugins import PluginConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class PluginApp(PluginConfig):
     name = "ephios_signup_beachguard"
 
     class EphiosPluginMeta:
-        name = "ephios_signup_beachguard"
+        name = _("Lifeguard Signup")
         author = "Julian Baumann <julian@ephios.de>"
-        description = "This plugin provides a signup method for beach guards where there are regular shifts which all share the same configuration"
+        description = _("This plugin provides a signup method for beach guards where there are regular shifts which all share the same configuration")
 
     def ready(self):
         from . import signals  # NOQA
